0d7e2648618dc301957d40060ef032d5f3f6a112
demiacle <demiacle@gmail.com>
2016-12-06 12:31:08 -0800

    First source commit

---
+++ src/main/java/com/anysoftkeyboard/languagepack/languagepack/PackBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.anysoftkeyboard.languagepack.languagepack;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class PackBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        //Toast.makeText( arg0.getApplicationContext(), "Intent Detected.", Toast.LENGTH_LONG ).show();
+    }
+
+}
